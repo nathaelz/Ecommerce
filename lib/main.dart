4a39fde3cbc67ce4d4ec4f883676f509843f2f46
nathaelz <nathaliemarie2303@gmail.com>
2022-10-12 17:03:06 -0400

    app finish 0.2

--- lib/main.dart
+++ lib/main.dart
@@ -4,14 +4,10 @@ import 'package:sellar_e_commerce/eCommerce/auth/register_screen.dart';
 import 'package:sellar_e_commerce/eCommerce/your_account_scren.dart';
 import 'package:sellar_e_commerce/widget_tree.dart';
 
-<<<<<<< HEAD
 import 'eCommerce/app_provider.dart';
 import 'eCommerce/auth/login_screen.dart';
 import 'eCommerce/services/service.dart';
 import 'eCommerce/shopping_cart_screen.dart';
-=======
-import 'eCommerce/auth/login_page.dart';
->>>>>>> a7b4f8cb550ccf5ee5234f20fb506a608d01b67e
 
 void main() {
   runApp(MyApp());
@@ -20,7 +16,6 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-<<<<<<< HEAD
     return MultiProvider(
         providers: [
           ChangeNotifierProvider(create: (_) => BEServices()),
@@ -42,16 +37,5 @@ class MyApp extends StatelessWidget {
               iconTheme: IconThemeData(color: Colors.black.withOpacity(0.4))),
           home: WidgetTree(),
         ));
-=======
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Sellar eCommerce',
-      theme: ThemeData(
-          primaryColor: Color(0xFF283C63),
-          accentColor: Color(0xFFE7E9F5),
-          iconTheme: IconThemeData(color: Colors.black.withOpacity(0.4))),
-      home: LoginScreen(),
-    );
->>>>>>> a7b4f8cb550ccf5ee5234f20fb506a608d01b67e
   }
 }
